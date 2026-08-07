69d2a6c95db6cd352818b78c1cd2431ad146a32d
Prince <prince79003@gmail.com>
2026-08-07 12:45:30 +0530

    Add solution for 132 Pattern using monotonic stack

---
+++ Stack/LC456-32 Pattern.cpp
@@ -0,0 +1,23 @@
+//Time Complexity-- O(N)
+//Space Complexity-- O(N)
+
+class Solution {
+public:
+    bool find132pattern(vector<int>& nums) {
+        int n= nums.size();
+        int nums3= INT_MIN;
+        stack<int>st;
+        for(int i=n-1;i>=0;i--){
+            if(nums[i]<nums3){
+                return true;
+            }
+            while(!st.empty()&& st.top()<nums[i]){
+                nums3=st.top();
+                st.pop();
+            }
+            st.push(nums[i]);
+        }
+        return false;
+        
+    }
+};
